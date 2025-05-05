eaa330225d3199be01cc39f45c80a54fa3a264e6
Ryan Sinnet <rsinnet@gmail.com>
2025-05-05 11:23:04 -0700

    Remove requirement on pybullet
    Don't fail on ImportError. The pybullet package is not used by
    all functionality.

--- onshape_to_robot/__init__.py
+++ onshape_to_robot/__init__.py
@@ -1,5 +1,8 @@
 """Export models from OnShape to other robotics formats."""
-from . import bullet  # noqa: F401
+try:
+    from . import bullet  # noqa: F401
+except ImportError:
+    pass
 from . import clear_cache  # noqa: F401
 from . import edit_shape  # noqa: F401
 from . import onshape_to_robot  # noqa: F401
